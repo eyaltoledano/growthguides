8c6d811f5ff89ccaf263b00cb011a98783bd0e05
Eyal Toledano <etoledano@playigl.com>
2015-02-24 14:00:04 -0500

    Added dashboard, added free guides support

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
+
+before_action :authenticate_user!, only: [:dashboard]
   
   def home
   	if current_user
   	redirect_to guides_path
   	end
+  end
+
+  def dashboard
+  	@guide = current_user.guides
   end
 
 end
